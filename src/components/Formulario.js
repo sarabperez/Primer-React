e9305f17c95cb3b3142406d6e99ea243f7cf0b47
Sara Perez <sarabelenpz@gmail.com>
2024-03-17 11:55:23 -0300

    :primer cambio

---
+++ src/components/Formulario.js
@@ -0,0 +1,6 @@
+import React from "react";
+export const Formulario =() =>{
+    return (
+        <h1>Hello Dojo!</h1>
+    )
+}
---
+++ src/components/Lista.js
@@ -0,0 +1,14 @@
+import React from "react";
+export const Lista=() =>{
+    return(
+        <div>
+            <h2>Things I need to do</h2>
+            <ul>
+                <li>Learn React </li>
+                <li>Climb Mt.Everest </li>
+                <li>Run a marathon </li>
+                <li>Feed the dogs </li>
+            </ul>
+        </div>
+    )
+}
